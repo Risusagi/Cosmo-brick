e56c202fd1177cfcc2893a709198b7dc0efc6b4a
Risusagi <anastazja199707@gmail.com>
2019-08-25 12:03:29 +0200

    webpack part 2

---
+++ webpack.common.js
@@ -0,0 +1,19 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+module.exports = {
+    entry: "./src/js/index.js",
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    },
+};
