c8e1e0cd8f09eaeaf831b2e95666bb96c92bc587
Risusagi <anastazja199707@gmail.com>
2019-09-01 18:56:46 +0200

    bricks' style + icon

--- src/js/Brick.js
+++ src/js/Brick.js
@@ -19,33 +19,40 @@ export default class Brick {
         }
     }
     draw(c) {
-        c.fillStyle = "black";
         switch (this.strength) {
             case 5:
-                c.fillStyle = '#f90000';
+                c.fillStyle = '#c70202';
+                c.strokeStyle = '#830202';
+                this.innerStyle = '#f90000';
                 break;
             case 4:
-                c.fillStyle = '#f600ff';
-                
+                c.fillStyle = '#c100c8';
+                c.strokeStyle = '#930098';
+                this.innerStyle = '#f600ff';
                 break;
             case 3:
-                c.fillStyle = '#f15500';
+                c.fillStyle = '#d86702';
+                c.strokeStyle = '#a83b00';
+                this.innerStyle = '#ff7800';
                 break;
             case 2:
-                c.fillStyle = '#fff000';
+                c.fillStyle = '#e6d600';
+                c.strokeStyle = '#b1a201';
+                this.innerStyle = '#ffff1a';
                 break;
             case 1:
                 c.fillStyle = '#00aa11';
+                c.strokeStyle = '#007d0c';
+                this.innerStyle = '#3fff00';
                 break;
         }
 
         
         c.fillRect(this.x, this.y, this.width, this.height);
 
-        c.fillStyle = '#3fff00';
+        c.fillStyle = this.innerStyle;
         c.fillRect(this.x + 3, this.y + 3, this.width - 6, this.height - 6);
-
-        c.strokeStyle = '#007d0c';
+        
         c.strokeRect(this.x + 5, this.y + 5, this.width - 10, this.height - 10);
         c.lineWidth = 1;
     }
